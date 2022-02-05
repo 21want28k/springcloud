52795bd9cf7284934445fdf566e09e631dd43a29
21want28k <937930940@qq.com>
2022-02-05 19:00:03 +0800

    12

---
+++ cloudalibaba-config-nacos-client-3377/src/main/java/com/xx/springcloud/ConfigNacosMain3377.java
@@ -0,0 +1,14 @@
+package com.xx.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class ConfigNacosMain3377 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigNacosMain3377.class,args);
+    }
+}
